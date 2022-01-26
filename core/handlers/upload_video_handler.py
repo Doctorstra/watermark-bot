cf8704846b733e90809f85da46fc3a6d7b14e594
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2022-01-27 02:26:43 +0530

    Update upload_video_handler.py

--- core/handlers/upload_video_handler.py
+++ core/handlers/upload_video_handler.py
@@ -19,9 +19,9 @@ async def send_video_handler(bot, cmd, output_vid, video_thumbnail, duration, wi
         height=height,
         reply_to_message_id=cmd.message_id,
         supports_streaming=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/AbirHasan2005")],
-                                           [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")],
-                                           [InlineKeyboardButton("Support Group", url="https://t.me/linux_repo")]]),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/Doctorstra")],
+                                           [InlineKeyboardButton("Bots Channel", url="https://t.me/Dads_links")],
+                                           [InlineKeyboardButton("Support Group", url="https://t.me/Dads_links")]]),
         progress=progress_for_pyrogram,
         progress_args=(
             "Uploading, Wait Sir ...",
